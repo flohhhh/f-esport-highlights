0cb37c83eeec03c4e3d5feeca4c3b429115eb7f3
Florian Pires <florian.pires@agicap.com>
2023-05-31 15:04:24 +0200

    feat: expo-router + reanimated + jotai + axios + prettier

---
+++ app/home/index.tsx
@@ -0,0 +1,6 @@
+import {View} from "react-native";
+
+const Index = ()=> {
+  return <View style={{flex: 1, backgroundColor: 'blue'}}></View>
+}
+export default Index
---
+++ app/index.tsx
@@ -0,0 +1,6 @@
+import { View } from 'react-native'
+
+const Index = () => {
+  return <View style={{ flex: 1, backgroundColor: 'red' }}></View>
+}
+export default Index
